c863560c116414759fad8c6211cf0bb23630e517
Rachel Russell <99039149+ror16sd@users.noreply.github.com>
2022-11-23 16:29:08 -0500

    Java Files for Superhero (update files) [TEMPLATE]

--- Superhero/src/main/java/superhero/App.java
+++ Superhero/src/main/java/superhero/App.java
@@ -1,7 +1,7 @@
 package superhero;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
+
 @SpringBootApplication
 public class App {
 
