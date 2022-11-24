fe56c8607399b96d9554bfb72b7ac3290c04b04c
Cirũ Franklin <cirufrank@me.com>
2022-11-23 17:14:21 -0800

    rename SuperPowersController to SuperPowerController

--- Superhero/src/main/java/superhero/controller/SuperPowerController.java
+++ Superhero/src/main/java/superhero/controller/SuperPowerController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/super-powers")
-public class SuperPowersController {
+public class SuperPowerController {
     
     @GetMapping
     public String getSuperPowers() {
