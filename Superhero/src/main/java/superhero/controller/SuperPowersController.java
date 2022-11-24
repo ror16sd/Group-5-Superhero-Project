7e62dbef6a957259a5cbdf8eab2bf19b90eec730
Cirũ Franklin <cirufrank@me.com>
2022-11-23 17:10:07 -0800

    SPlit application controller into separate files for separate concerns

---
+++ Superhero/src/main/java/superhero/controller/SuperPowersController.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package superhero.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author ciruf
+ */
+@RestController
+@RequestMapping("/api/super-powers")
+public class SuperPowersController {
+    
+    @GetMapping
+    public String getSuperPowers() {
+        return "NOT IMPLEMENTED: View super powers";
+    }
+    
+    @PostMapping
+    public String createSuperPower() {
+        return "NOT IMPLEMENTED: Create super powers";
+    }
+    
+    @GetMapping("/{id}")
+    public String getSuperPower(@PathVariable int id) {
+        return "NOT IMPLEMENTED: Get specific super power";
+    }
+    
+    @PutMapping("/{id}")
+    public String updateSuperPower(@PathVariable int id) {
+        return "NOT IMPLEMENTED: Update specific super power";
+    }
+    
+    @DeleteMapping("/{id}")
+    public String deleteSuperPower(@PathVariable int id) {
+        return "NOT IMPLEMENTED: Delete specific super power";
+    }
+    
+}
---
+++ Superhero/src/main/java/superhero/controller/SuperSightingController.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package superhero.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author ciruf
+ */
+@RestController
+@RequestMapping("/api/super-sightings")
+public class SuperSightingController {
+    
+    @GetMapping
+    public String getSuperSightings() {
+        return "NOT IMPLEMENTED: View super sightings";
+    }
+    
+    @PostMapping
+    public String createSuperSighting() {
+        return "NOT IMPLEMENTED: Create super sighting";
+    }
+    
+    @GetMapping("/{id}")
+    public String getSuperSighting(@PathVariable int id) {
+        return "NOT IMPLEMENTED: Get specific super sighting";
+    }
+    
+    @PutMapping("/{id}")
+    public String updateSuperSighting(@PathVariable int id) {
+        return "NOT IMPLEMENTED: Update specific super sighting";
+    }
+    
+    @DeleteMapping("/{id}")
+    public String deleteSuperSighting(@PathVariable int id) {
+        return "NOT IMPLEMENTED: Delete specific super sighting";
+    }
+}
