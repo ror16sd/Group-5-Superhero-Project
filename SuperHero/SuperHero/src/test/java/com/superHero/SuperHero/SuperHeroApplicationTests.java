d1be460f23a98c42dc8a6ac25d70d011b0e2ed47
Blasian_Dumpling <94583587+BLASIAN-DUMPLING@users.noreply.github.com>
2022-11-27 15:36:15 -0600

    Add files via upload
    updated Front End

---
+++ SuperHero/SuperHero/src/test/java/com/superHero/SuperHero/SuperHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superHero.SuperHero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
