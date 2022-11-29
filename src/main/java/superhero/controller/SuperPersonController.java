7aef5d6723a2a7e5e1671fcc0499f3b84bcbf9f0
Cirũ Franklin <cirufrank@me.com>
2022-11-29 13:48:17 -0800

    Upddate WHO.html file to include selects for powers and true or false, and update SuperPersonCOntroller to pass that information

--- src/main/java/superhero/controller/SuperPersonController.java
+++ src/main/java/superhero/controller/SuperPersonController.java
@@ -4,7 +4,12 @@
  */
 package superhero.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +17,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import superhero.dao.PowerDao;
+import superhero.model.Power;
+import superhero.model.Super;
 
 /**
  *
@@ -20,14 +28,25 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 @RequestMapping("/super-people")
 public class SuperPersonController {
+    
+    
+   @Autowired
+   PowerDao powerDao;
+   
    @GetMapping
-    public String getSuperPeople() {
+    public String getSuperPeople(Model model) {
+//        In the future
+//        List<Power> powers = powerDao.getAllPowers();
+        List<Power> powers = testPowerList();
+        model.addAttribute("powers", powers);
         return "Who";
     }
     
     @PostMapping
-    public String createSuperPerson() {
-        return "NOT IMPLEMENTED: Create super person";
+    public String createSuperPerson(Super superPerson, HttpServletRequest request, Model model) {
+        final boolean isHero = Boolean.parseBoolean(request.getParameter("isHero"));
+        final Power power = powerDao.getPowerById(Integer.parseInt(request.getParameter("powerId")));
+        return "redirect:/Who";
     }
     
     @GetMapping("/{id}")
@@ -43,5 +62,23 @@ public class SuperPersonController {
     @DeleteMapping("/{id}")
     public String deleteSuperPerson(@PathVariable int id) {
         return "NOT IMPLEMENTED: Delete specific super person";
+    }
+    
+    //TEST METHODS
+    private List<Power> testPowerList() {
+        List<Power> powers = new ArrayList<>();
+        Power testPow1 = new Power();
+        testPow1.setPowerId(1);
+        testPow1.setPowerDescription("Super Strength");
+        Power testPow2 = new Power();
+        testPow2.setPowerId(2);
+        testPow2.setPowerDescription("Invisibility");
+        Power testPow3 = new Power();
+        testPow3.setPowerId(3);
+        testPow3.setPowerDescription("Omniscence");
+        powers.add(testPow1);
+        powers.add(testPow2);
+        powers.add(testPow3);
+        return powers;
     }
 }
