5072e05fef2f56995356a1648d16572ba3fb194b
Blasian_Dumpling <94583587+BLASIAN-DUMPLING@users.noreply.github.com>
2022-11-29 10:48:35 -0600

    Add files via upload
    updated frontend

---
+++ templates/home.js
@@ -0,0 +1,26 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
+ */
+
+document.querySelector("#files").addEventListener("change", (e) => {
+  if(window.File && window.FileReader && window.FileList && window.Blob){
+        const files = e.target.files;
+        const output = document.querySelector("#result");
+
+        for(let i = 0; i < files.length;i++){
+            if(!files[i].type.match("image")) continue;
+            const picReader = new FileReader();
+            picReader.addEventListener("load", function(event){
+                const picFile = event.target;
+                const div = document.createElement("div");
+                div.innerHTML = `<img class="thumbnail" src="${picFile.result}" title="${picFile.name}"/>`;
+                output.appendChild(div);
+            }) 
+        picReader.readAsDataURL(files[i]);
+    }
+
+      } else {
+        alert("Your Browser Does Not Support the File API")
+      }
+});
