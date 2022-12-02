3cac38513fd8182c0ad6ede69fded640d3ef3dfa
Cirũ Franklin <cirufrank@me.com>
2022-12-01 18:07:11 -0800

    Update HTML pages I'm responsible for to have their naviation links, link correctly, and implement for validation for HomeBase.html page and LocationController

--- src/main/java/superhero/controller/LocationController.java
+++ src/main/java/superhero/controller/LocationController.java
@@ -4,11 +4,21 @@
  */
 package superhero.controller;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import superhero.dao.LocationDao;
+import superhero.model.Location;
+import superhero.model.Super;
 
 /**
  *
@@ -17,12 +27,28 @@ import superhero.dao.LocationDao;
 @Controller
 @RequestMapping("/locations")
 public class LocationController {
+    Set<ConstraintViolation<Location>> violations = new HashSet<>();
     
     @Autowired
     LocationDao locationDao;
     
     @GetMapping
-    public String getLocations() {
+    public String getLocations(Model model) {
+        final List<Location> locations = locationDao.getAllLocations();
+        model.addAttribute("locations", locations);
+        model.addAttribute("errors", violations);
         return "HomeBase.html";
+    }
+       
+    @PostMapping
+    public String createLocation(Location location) {
+      Validator validate = Validation.buildDefaultValidatorFactory().getValidator();
+      violations = validate.validate(location);  
+      
+      if (violations.isEmpty()) {
+           locationDao.addLocation(location);
+       }
+      
+      return "redirect:/locations";
     }
 }
